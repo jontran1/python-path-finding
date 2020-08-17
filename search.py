ae14d84e26d117ef31df2291e547747083513eae
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-08-17 17:22:24 -0400

    Added comments to BFS.

--- search.py
+++ search.py
@@ -1,4 +1,10 @@
 def breadth_first_search(draw, start, end):
+    """
+        draw: lambda function. This function is called each while loop iteration. 
+        This will update the grid for the user.
+        start (Node): A starting node.
+        end (Node): A ending node.
+    """
     queue = []
     visited = []
     queue.append(start)
